838f0536d3cd3a0a8eda2995507174a2966513b3
renren-017 <altynai.mamytova17@gmail.com>
2022-04-10 18:23:34 +0600

    Base template changed

--- chatroom/forms.py
+++ chatroom/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from chatroom.models import Message
 
 class TextMessageForm(forms.Form):
-    text_message = forms.CharField(widget=forms.Textarea(attrs={"rows":10}))
+    text_message = forms.CharField(widget=forms.Textarea(attrs={"rows":4}))
     class Meta:
         model = Message
         fields = ['content']
